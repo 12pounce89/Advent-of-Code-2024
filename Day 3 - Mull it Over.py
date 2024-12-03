26870f28f82965db6d43459c7993787e4d209830
Alex Storrs <astorrs27@gmail.com>
2024-12-03 12:27:00 -0500

    Day 3 complete (formatting edit)

--- Day 3 - Mull it Over.py
+++ Day 3 - Mull it Over.py
@@ -1,11 +1,32 @@
+'''
 # Part 1
 
 import re
 
 correct = []
 sum = 0
+
+with open('data/day3.txt') as file:
+    for line in file:
+        correct += re.findall("mul\(\d+,\d+\)", line)
+
+for item in correct:
+    broken = item.split(',')
+    first = broken[0].split('(')
+    second = broken[1].split(')')
+    mult = int(first[1]) * int(second[0])
+    sum += mult
+
+print(sum)
+'''
+
+# Part 2
+
+import re
+
+correct = []
+sum = 0
 on = True
-testSum = 0
 
 with open('data/day3.txt') as file:
     for line in file:
@@ -22,12 +43,5 @@ for item in correct:
         second = broken[1].split(')')
         mult = int(first[1]) * int(second[0])
         sum += mult
-    elif not on:
-        broken = item.split(',')
-        first = broken[0].split('(')
-        second = broken[1].split(')')
-        mult = int(first[1]) * int(second[0])
-        testSum += mult
 
 print(sum)
-print(testSum)
