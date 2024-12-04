26ae309e0cc496877949334163801c81875efbd2
12pounce89 <astorrs27@gmail.com>
2024-12-04 10:46:40 -0500

    Day 4 complete

--- Day 4 - Ceres Search.py
+++ Day 4 - Ceres Search.py
@@ -1,9 +1,10 @@
+'''
 # Part 1
 
 grid = []
 count = 0
 
-with open('data/tester.txt') as file:
+with open('data/day4.txt') as file:
     for line in file:
         letters = list(line)
         grid.append(letters)
@@ -34,35 +35,35 @@ for i in range(len(grid)):
                     newGrid[i + 1][j] = 'M'
                     newGrid[i + 2][j] = 'A'
                     newGrid[i + 3][j] = 'S'
-            if ((j - 3) > 0 and (i + 3) < len(grid)):
+            if ((j - 3) >= 0 and (i + 3) < len(grid)):
                 if grid[i + 1][j - 1] == 'M' and grid[i + 2][j - 2] == 'A' and grid[i + 3][j - 3] == 'S':
                     count += 1
                     newGrid[i][j] = 'X'
                     newGrid[i + 1][j - 1] = 'M'
                     newGrid[i + 2][j - 2] = 'A'
                     newGrid[i + 3][j - 3] = 'S'
-            if ((j - 3) > 0):
+            if ((j - 3) >= 0):
                 if grid[i][j - 1] == 'M' and grid[i][j - 2] == 'A' and grid[i][j - 3] == 'S':
                     count += 1
                     newGrid[i][j] = 'X'
                     newGrid[i][j - 1] = 'M'
                     newGrid[i][j - 2] = 'A'
                     newGrid[i][j - 3] = 'S'
-            if ((j - 3) > 0 and (i - 3) > 0):
+            if ((j - 3) >= 0 and (i - 3) >= 0):
                 if grid[i - 1][j - 1] == 'M' and grid[i - 2][j - 2] == 'A' and grid[i - 3][j - 3] == 'S':
                     count += 1
                     newGrid[i][j] = 'X'
                     newGrid[i - 1][j - 1] = 'M'
                     newGrid[i - 2][j - 2] = 'A'
                     newGrid[i - 3][j - 3] = 'S'
-            if ((i - 3) > 0):
+            if ((i - 3) >= 0):
                 if grid[i - 1][j] == 'M' and grid[i - 2][j] == 'A' and grid[i - 3][j] == 'S':
                     count += 1
                     newGrid[i][j] = 'X'
                     newGrid[i - 1][j] = 'M'
                     newGrid[i - 2][j] = 'A'
                     newGrid[i - 3][j] = 'S'
-            if ((j + 3) < len(grid[i]) and (i - 3) > 0):
+            if ((j + 3) < len(grid[i]) and (i - 3) >= 0):
                 if grid[i - 1][j + 1] == 'M' and grid[i - 2][j + 2] == 'A' and grid[i - 3][j + 3] == 'S':
                     count += 1
                     newGrid[i][j] = 'X'
@@ -73,7 +74,42 @@ for i in range(len(grid)):
 
 
 print(count)
-for item in newGrid:
-    for char in item:
-        print(char, end='')
-    print()
+# for item in newGrid:
+#     for char in item:
+#         print(char, end='')
+#     print()
+'''
+
+# Part 2
+
+grid = []
+count = 0
+
+with open('data/day4.txt') as file:
+    for line in file:
+        letters = list(line)
+        grid.append(letters)
+
+newGrid = [['.' for i in range(len(grid[0]))] for i in range(len(grid))]
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] == 'M':
+            if j + 2 < len(grid[i]) and i + 2 < len(grid):
+                if (grid[i][j + 2] == 'M' and grid[i + 1][j + 1] == 'A' and grid[i + 2][j] == 'S' and grid[i + 2][j + 2] == 'S'):
+                    count += 1
+                elif (grid[i + 2][j] == 'M' and grid[i + 1][j + 1] == 'A' and grid[i][j + 2] == 'S' and grid[i + 2][j + 2] == 'S'):
+                    count += 1
+            if i - 2 >= 0 and j + 2 < len(grid[i]):
+                if (grid[i][j + 2] == 'M' and grid[i - 1][j + 1] == 'A' and grid[i - 2][j] == 'S' and grid[i - 2][j + 2] == 'S'):
+                    count += 1
+            if i + 2 < len(grid) and j - 2 >= 0:
+                if (grid[i + 2][j] == 'M' and grid[i + 1][j - 1] == 'A' and grid[i][j - 2] == 'S' and grid[i + 2][j - 2] == 'S'):
+                    count += 1
+
+
+print(count)
+# for item in newGrid:
+#     for char in item:
+#         print(char, end='')
+#     print()
